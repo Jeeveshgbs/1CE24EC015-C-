de85e481091e089b8cda7756721550458baee063
Jeeveshgbs <jeeveshgbs@gmail.com>
2025-05-29 16:00:46 +0530

    Add files via upload

---
+++ exception.handling.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    try
+    {
+        if(num < 0)
+            throw "Negative number entered!";
+            cout<<"You entered: "<<num<<endl;
+
+
+    }
+    catch(const char *msg)
+    {
+        cout<<"Exception: "<<msg<<endl;
+    }
+    return 0;
+}
